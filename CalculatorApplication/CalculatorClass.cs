3d81435b2dbaeffb653e1aa8007f6c8271e40aef
Rio Galura <riounjanegalura@gmail.com>
2025-08-22 11:45:48 +0800

    declare generic

---
+++ CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+
+    internal class CalculatorClass
+    {
+        public delegate T Formula<T>(T arg1);
+    }
+}
